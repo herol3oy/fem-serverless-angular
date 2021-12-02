6ad370fac4aee4ec678158932bdff612c2ac7e26
Hamed Sedighi <hamedsedighi@gmail.com>
2021-12-02 20:25:58 +0100

    update hello

---
+++ netlify/functions/hello.js
@@ -0,0 +1,10 @@
+exports.handler = async (event, context) => {
+  console.log("Hello Angular World o(*ﾟ∇ﾟ)ﾉ");
+  console.log(`\nHere is the event info: ${JSON.stringify(event)}`);
+  console.log(`\nHere is the context info: ${JSON.stringify(context)}`);
+
+  return {
+    statusCode: 200,
+    body: "This function is totes working, YAY!",
+  };
+};
